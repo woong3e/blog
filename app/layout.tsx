d92d1b7fdcae50ccff83d0f6e937d6cee125ba4e
GUNWOONG JANG <gunwoong32@gmail.com>
2026-01-13 00:19:04 +0900

    chore add 배민도현체 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-import { Space_Grotesk } from 'next/font/google'
+import localFont from 'next/font/local'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -12,10 +12,11 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
+const BMDOHYEON = localFont({
+  src: '../public/fonts/BMDOHYEON_otf.otf',
+  weight: '400',
+  style: 'normal',
+  variable: '--font-bmdohyeon',
 })
 
 export const metadata: Metadata = {
@@ -64,7 +65,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang={siteMetadata.language}
-      className={`${space_grotesk.variable} scroll-smooth`}
+      className={`${BMDOHYEON.variable} scroll-smooth`}
       suppressHydrationWarning
     >
       <link
