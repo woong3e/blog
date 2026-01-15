32bc27496751d41d6dc931059aaf0710cc9f175f
GUNWOONG JANG <gunwoong32@gmail.com>
2026-01-15 14:26:53 +0900

    chore giscus 설정 변경

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -3,17 +3,34 @@
 import { Comments as CommentsComponent } from 'pliny/comments'
 import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
+import { useTheme } from 'next-themes'
 
 export default function Comments({ slug }: { slug: string }) {
   const [loadComments, setLoadComments] = useState(false)
+  const { resolvedTheme } = useTheme()
 
   if (!siteMetadata.comments?.provider) {
     return null
   }
+
+  const commentsConfig =
+    siteMetadata.comments?.provider === 'giscus'
+      ? {
+          ...siteMetadata.comments,
+          giscusConfig: {
+            ...siteMetadata.comments.giscusConfig,
+            theme:
+              resolvedTheme === 'dark'
+                ? siteMetadata.comments.giscusConfig.darkTheme
+                : siteMetadata.comments.giscusConfig.theme,
+          },
+        }
+      : siteMetadata.comments
+
   return (
     <>
       {loadComments ? (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+        <CommentsComponent commentsConfig={commentsConfig} slug={slug} />
       ) : (
         <button onClick={() => setLoadComments(true)}>Load Comments</button>
       )}
